8e2172c8c6b6ebd7b582471b74583d39bbddbb1c
Bach Tran <bachtran@kms-technology.com>
2026-06-23 16:02:00 +0700

    chore: remove extra whitespace

--- src/main/java/project/training/com/example/demo/config/flow/Flow.java
+++ src/main/java/project/training/com/example/demo/config/flow/Flow.java
@@ -6,22 +6,22 @@ import org.springframework.integration.dsl.IntegrationFlow;
 
 import project.training.com.example.demo.constants.channel.ChannelNames;
 
-    @Configuration
-    public class Flow {
+@Configuration
+public class Flow {
 
-        @Bean
-        public IntegrationFlow mainFlow() {
-            return IntegrationFlow.from(ChannelNames.INPUT_CHANNEL)
-                    .log(message -> "INPUT: " + message.getPayload())
-                    .channel(ChannelNames.DOMAIN_CHANNEL)
-                    .get();
-        }
-
-        @Bean
-        public IntegrationFlow errorFlow() {
-            return IntegrationFlow.from(ChannelNames.ERROR_CHANNEL)
-                    .log(message -> "ERROR: " + message.getPayload())
-                    .get();
-        }
+    @Bean
+    public IntegrationFlow mainFlow() {
+        return IntegrationFlow.from(ChannelNames.INPUT_CHANNEL)
+                .log(message -> "INPUT: " + message.getPayload())
+                .channel(ChannelNames.DOMAIN_CHANNEL)
+                .get();
     }
+
+    @Bean
+    public IntegrationFlow errorFlow() {
+        return IntegrationFlow.from(ChannelNames.ERROR_CHANNEL)
+                .log(message -> "ERROR: " + message.getPayload())
+                .get();
+    }
+}
 
